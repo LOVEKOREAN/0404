0bc23cb681bb0d624b92a07ab0e33b9d9807a36c
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-10-08 19:59:43 +0900

    06
    실습 6

--- main.c
+++ main.c
@@ -3,22 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned int x;
-    int b;
-    
-    printf("input a number :");
-    scanf("%ui",&x);
-    
-    for (b=0; x !=0; x >>=1)
-    {
-        if (x&1)
-        {
-                b++;
-        }
-    }
-    
-    printf("the result is : %i\n",b);
-    
+    int input;
+ int sec, min, hour;
+ 
+ printf("input second:\n");
+ scanf("%d", &input);
+ 
+ sec= input%60;
+ min= (input%3600)/60;
+ hour= input/3600;
+ 
+ printf("the time is %d : %d : %d\n", hour, min, sec);
+
+  
     
   
   system("PAUSE");	
